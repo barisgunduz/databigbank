6867f6378c4e680ebd8b63a831b69291c5e38a9f
Barış Gündüz <baris@gunduzmedya.com>
2023-10-12 20:55:18 +0300

    MUI Dashboard Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+/* import './App.css' */
+import Button from '@mui/material/Button';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +14,8 @@ function App() {
 
   return (
     <>
-      <div>
+    <Dashboard />
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -22,16 +25,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => addXToCounter(1)}>
+
+        <Button variant="contained" onClick={() => addXToCounter(1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
